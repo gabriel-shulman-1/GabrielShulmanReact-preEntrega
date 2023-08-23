2113e8b87d36b39201883b32184d2a220c42892b
gabriel-shulman-1 <ges.over@gmail.com>
2023-08-23 20:41:13 -0300

    se paso css a objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from "react"
 import carrito from "./components/assets/carrito.png";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { NavBar } from "./components/NavBar";
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,39 @@
 import carrito from "../components/assets/carrito.png";
 import pc from "../components/assets/pc.png"
 import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+
+const header = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#d8d8d8",
+}
+
+const navbar = {
+    display: "flex",
+    flexdirection: "row",
+    justifycontent: "center",
+    gap: "2rem",
+    borderradius: "0.5rem",
+}
+
+const a = {
+    backgroundColor: "#1c92f9",
+    margin: "1rem",
+    width: "auto",
+    padding: "0.5rem",
+    borderRadius: "0.6rem",
+}
+
 export const NavBar = () => {
     return (
-    <header>
+    <header style={header}>
         <img src={pc}></img>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <a class="navbar-brand" href="Hardware">Hardware</a>
-            <a class="navbar-brand" href="Software">Software</a>
-            <a class="navbar-brand" href="Accesorios">Accesorios</a>
+        <nav style={navbar} class="navbar-expand-lg bg-body-tertiary">
+            <a style = {a}class="navbar-brand" href="Hardware">Hardware</a>
+            <a style = {a} class="navbar-brand" href="Software">Software</a>
+            <a style = {a} class="navbar-brand" href="Accesorios">Accesorios</a>
         </nav>
         <img src={carrito}></img>
     </header>
