f79c438f09838d41d0cde7a91d53ce70b4fc8272
gabriel-shulman-1 <ges.over@gmail.com>
2023-08-28 20:39:57 -0300

    pre entrega terminada

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,11 +9,11 @@ export const NavBar = () => {
     return (
     <header>
         <img src={pc}></img>
-        <nav className="navbar-expand-lg bg-body-tertiary">
-            <a className="navbar-brand" href="/">Home</a>
-            <a className="navbar-brand" href="/category/hardware">Hardware</a>
-            <a className="navbar-brand" href="/category/software">Software</a>
-            <a className="navbar-brand" href="/category/accessories">Accesorios</a>
+        <nav className="navBar">
+            <a className="buttonNavBar" href="/">Home</a>
+            <a className="buttonNavBar" href="/category/hardware">Hardware</a>
+            <a className="buttonNavBar" href="/category/software">Software</a>
+            <a className="buttonNavBar" href="/category/accessories">Accesorios</a>
         </nav>
         <img src={carrito}></img>
     </header>
