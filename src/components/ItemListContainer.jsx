21800910b07f64d347b1422b157233677ec48114
Gabriel Shulman Laburo <ges.over@gmail.com>
2023-08-30 12:43:52 -0300

    se corrigio el loading

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,48 +2,36 @@ import { useState, useEffect } from "react";
 import { ItemCount } from "./ItemCount";
 import data from "../data/products.json";
 import { useParams } from "react-router-dom";
+import { ItemDetail } from "./ItemDetail";
 
 export function ItemListContainer(props){
 
     const [products,setProducts] = useState([])
     const {id} = useParams()
-    console.log(id)
+    
     useEffect(()=> {
         const promise = new Promise((resolve,reject)=>
-
+        
         {setTimeout(()=>resolve(data),2000)})
         
         promise.then((data)=> 
         {if (!id){
-            setProducts(data)  
-        } else {
-            const productsFiltered = data.filter(
+                setProducts(data)  
+            } else {
+                const productsFiltered = data.filter(
                     (product) => product.category===id
                     )
                     setProducts(productsFiltered)
-                }})
-            },[])
+                    }})
+                },[id])
             
-            if(!products) {
-                return(
-                    <h2>Loading...</h2>
-                )
-            }
+                if(products.length === 0) {return(<h2 className="loading">Loading...</h2>)}
 
     return (
         <>
             <div id="greeting" className="fw-bold">{props.greeting}</div>
             <div className="listContainer">
-                {products.map(product => (
-                        <div className="productCard" key={product.id}>
-                            <h3>{product.name}</h3>
-                            <p>Cantidad : {product.qty}</p>
-                            <p>Precio : {product.price} U$</p>
-                            <ItemCount/>
-                        </div>
-                        )
-                    )
-                }
+                <ItemDetail products={products}/>
             </div>
         </>
     )
