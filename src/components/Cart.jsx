d8002897ed1721a5309f33bc3c464b80894fcf27
Gabriel Shulman Laburo <ges.over@gmail.com>
2023-09-19 14:50:53 -0300

    primera barra de detalle en Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ export function Cart (){
     
     return(
         <>
-        
+            <div className="details"><p className="textDetail">Producto</p><p className="textDetail">Cantidad</p><p className="textDetail">Importe</p><p className="textDetail">Total</p></div>
         </>
     )
 }
